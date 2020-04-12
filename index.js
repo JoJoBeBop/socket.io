16b9d0788d2e6f09519a3c0dca3023cba921b443
JoJoBeBop <jori.blom@metropolia.fi>
2020-04-12 20:50:51 +0300

    User can be created

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const io = require('socket.io')(http);
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  const user = {id: socket.id, username: "Kakka"}
 
-  console.log('a user connected', socket.id + " + " + user.username);
+
+  console.log('a user connected', socket.id );
 
   socket.on('disconnect', () => {
     console.log('a user disconnected', socket.id);
--- public/chat.js
+++ public/chat.js
@@ -1,23 +1,36 @@
 'use strict';
 
 const socket = io();
-
+/*
 document.querySelector('.chatID').addEventListener('submit', (event) => {
   event.preventDefault();
   const inp = document.getElementById('m');
   socket.emit('chat message', inp.value);
   inp.value = '';
-});
+});*/
+
 
 document.querySelector('.userfield').addEventListener('submit', (event) => {
   event.preventDefault();
-  const inp = document.getElementById('user');
-  socket.emit('user created', inp.value);
+  const nameInput = document.getElementById('user');
+  const messageInput = document.getElementById('m');
+
+  let newName;
+  if (nameInput.value) {
+    newName =  nameInput.value;
+  }
+
+  let messageAndUser = [];
+
+  messageAndUser.push(newName + ": " + messageInput.value);
+
+  socket.emit('chat message', messageAndUser);
+  console.log(messageAndUser);
   inp.value = '';
 });
 
-socket.on('chat message', (msg, usr) => {
+socket.on('chat message', (msg) => {
   const item = document.createElement('li');
-  item.innerHTML =  usr + ": " + msg;
+  item.innerHTML =msg;
   document.getElementById('messages').appendChild(item);
 });
